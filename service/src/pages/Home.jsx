8c6633884be5d8e79c11f76898722cc97fd503a8
kristin-inhwan <jih@kristinkorea.com>
2025-04-28 13:15:33 +0900

    fix: styled-components -> emotion react 방식으로 변경

--- service/src/pages/Home.jsx
+++ service/src/pages/Home.jsx
@@ -1,5 +1,27 @@
-import styled, { keyframes } from "styled-components";
+/** @jsxImportSource @emotion/react */
+import { keyframes } from "@emotion/react";
 import logo from "../assets/images/logo.svg";
+
+export default function Home() {
+  return (
+    <div style={appContainer}>
+      <header style={appHeader}>
+        <img src={logo} alt="logo" style={appLogo} />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={appLink}
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 // 로고 회전 애니메이션
 const AppLogoSpin = keyframes`
@@ -11,53 +33,30 @@ const AppLogoSpin = keyframes`
   }
 `;
 
-// 스타일 컴포넌트들
-const AppContainer = styled.div`
-  text-align: center;
-`;
+// 스타일 객체들
+const appContainer = {
+  textAlign: "center",
+};
 
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
+const appHeader = {
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white",
+};
 
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
+const appLogo = {
+  height: "40vmin",
+  pointerEvents: "none",
+  "@media (prefers-reduced-motion: no-preference)": {
+    animation: `${AppLogoSpin} infinite 20s linear`,
+  },
+};
 
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${AppLogoSpin} infinite 20s linear;
-  }
-`;
-
-const AppLink = styled.a`
-  color: #61dafb;
-`;
-
-function Home() {
-  return (
-    <AppContainer>
-      <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </AppHeader>
-    </AppContainer>
-  );
-}
-
-export default Home;
+const appLink = {
+  color: "#61dafb",
+};
